4f28f96bf0aef62d10068eec40def89e8c4a01a0
wshunter <whunter554@gmail.com>
2023-03-09 20:34:00 -0800

    unconstrained convex relaxation solver

--- geom.py
+++ geom.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import tqdm
 
 def rotmat(th):
     c=np.cos(th)
@@ -10,6 +10,12 @@ def delrotmat(th):
     s=np.sin(th)
     return np.asarray([[-s,c],[-c,-s]])
 
+def rotmatC(cs):
+    return np.asarray([[cs[0],cs[1]],[-cs[1],cs[0]]])
+
+def delrotmatC(cs):
+    return np.asarray([[-cs[1],cs[0]],[-cs[0],-cs[1]]])
+    
 def H_between2(xi,xj,meas,CINV=np.identity(3),no_jac = False):
     #loss
     delta = np.zeros((3,1))
@@ -48,3 +54,136 @@ def H_prior2(xi,meas,CINV=np.identity(3),no_jac = False):
     #loss jacobian
     del_H_i = np.identity(3)
     return (deltc @ delta)[0,0], 2*deltc @ del_H_i
+
+# full jacobian of measurement state
+def H_graph(state, meas, no_jac=False):
+    loss = 0.0
+    grad_H  = np.zeros_like(state)
+    for m in meas:
+        if m[0] == 'b':
+            #pose-pose measurement
+            if no_jac:
+                loss += H_between2(state[:,m[1]],state[:,m[2]],meas[m],CINV=CINV,no_jac=no_jac)
+            else:
+                l, jac_i, jac_j = H_between2(state[:,m[1]],state[:,m[2]],meas[m],CINV=CINV,no_jac=no_jac)
+                grad_H[:,m[1]] += jac_i[0]
+                grad_H[:,m[2]] += jac_j[0]
+                loss += l
+        if m[0] == 'p':
+            #prior
+            if no_jac:
+                loss += H_prior2(state[:,m[1]], meas[m], CINV=CINV,no_jac=no_jac)
+            else:
+                l, jac_i = H_prior2(state[:,m[1]], meas[m], CINV=CINV,no_jac=no_jac)
+                grad_H[:,m[1]] += jac_i[0]
+                loss += l
+    if no_jac:
+        return loss
+    else:
+        return loss, grad_H
+
+
+
+SO2_COS_INNER_DERIVATIVE = np.identity(2)
+SO2_SIN_INNER_DERIVATIVE = np.asarray([[0,1],[-1,0]])
+
+def H_between2_conv(xi,xj,meas,alpha=1.0,beta=1.0):
+    RJ = rotmatC(xj[2:])
+    RI = rotmatC(xi[2:])
+    RIJ = rotmatC(meas[2:,0])
+    DELTA_R = RJ - RIJ @ RI
+    alpha_term = alpha * np.sum(np.square(DELTA_R))
+    
+    del_pos = xj[:2] - xi[:2] - RI @ meas[:2,0]
+    beta_term = beta * np.sum(np.square(del_pos))
+    loss = alpha_term + beta_term
+
+    del_Hc_j = np.zeros((1,4))
+    #derivative wrt xj, yj
+    del_Hc_j[0,:2] = 2*beta*del_pos
+
+    del_Rj_inner = 2*alpha*(RJ - RIJ @ RI)
+    #derivative wrt cj
+    del_Hc_j[0,2] = np.tensordot(del_Rj_inner, SO2_COS_INNER_DERIVATIVE)
+    #derivative wrt sj
+    del_Hc_j[0,3] = np.tensordot(del_Rj_inner, SO2_SIN_INNER_DERIVATIVE)
+
+    del_Hc_i = np.zeros((1,4))
+    #derivative wrt xi, yi
+    del_Hc_i[0,:2] = -2*beta*del_pos
+    
+    #derivative wrt ci
+    #first term is from rotation loss, second is from translation
+    trans_outer = np.outer(xi[:2]-xj[:2] + RI @ meas[:2,0],meas[:2,0])
+    del_Ri_inner = 2*alpha*(RI - RIJ.T @ RJ) + 2*beta*trans_outer
+                                  
+    del_Hc_i[0,2] = np.tensordot(del_Ri_inner , SO2_COS_INNER_DERIVATIVE)
+    #derivative wrt si
+    del_Hc_i[0,3] = np.tensordot(del_Ri_inner, SO2_SIN_INNER_DERIVATIVE)
+    return loss, del_Hc_i, del_Hc_j
+
+def H_graph_conv(state, meas,alpha=1.0,beta=1.0):
+    loss = 0.0
+    delta = np.zeros((state.shape))
+    for m in meas:
+        if m[0] == 'b':
+            l, dHi, dHj =  H_between2_conv(state[:,m[1]],state[:,m[2]],meas[m],alpha,beta)
+            loss += l
+            delta[:,m[1]] += dHi[0,:]
+            delta[:,m[2]] += dHj[0,:]
+    return loss, delta
+
+
+def solve_graph_convex(x0, measC, alpha=1.0, beta=1.0, n_steps = 1000, return_states=True):
+
+    ybound = np.inf
+    xsh = x0.shape
+    lam = 1.0
+    losses = []
+    lams = []
+
+    alpha=1.0
+    beta=1.0
+
+    if return_states:
+        states = [x0]
+    print(f"\nOptimizing {x0.shape[1]} poses with {len(measC.keys())} constraints\n")
+    pbar = tqdm.trange(2000)
+    for i in pbar:
+        loss, grad = H_graph_conv(x0, measC)
+
+        #don't let constant term move 
+        grad[:,0] = 0.0
+        losses.append(loss)
+        update_state = True
+        while(update_state):
+
+            if lam < 1e-20:
+                break
+            #I am taking the lambda term outside of the pinv as opposed to normal LMQ (so we decrease lambda on a failure) since I find it more natural to formulate the objective function this way.
+
+            upd = x0 - (np.linalg.pinv(grad.reshape((1,-1))) * lam*loss).reshape(xsh)
+            #upd = x0 - lam*grad
+
+            ybound = H_graph_conv(upd, measC)[0]
+
+            #print(f"Tried lam {lam}, {ybound} vs {loss}")
+            if ybound >= loss:
+                lam *= 1e-1
+            else:
+                update_state = False
+                lams.append(lam)
+        pbar.set_description(f"{loss:.3f}")
+        lam *= 2
+        #print("succeeded")
+
+        x0 = upd
+        if return_states:
+            states.append(np.copy(x0))
+            
+    print("")
+
+    if return_states:
+        return x0, losses, lams, states
+    else:
+        return x0, losses, lams
--- solver.py
+++ solver.py
@@ -1,23 +1,27 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import tqdm
-from geom import rotmat, delrotmat, H_between2, H_prior2
+from geom import rotmat, delrotmat, H_between2, H_prior2, rotmatC, H_graph, H_graph_conv, solve_graph_convex
+from convex import hull_so2, graph_hull_so2#, H_between2_conv
 
-
-N = 31
+N = 101
 T = np.arange(N)
-pos = np.vstack((np.cos(T*2*np.pi/30 - np.pi/2),np.sin(T*2*np.pi/30 - np.pi/2),T*2*np.pi/30))
+pos = np.vstack((np.cos(T*2*np.pi/(N-1) - np.pi/2),np.sin(T*2*np.pi/(N-1) - np.pi/2),T*2*np.pi/(N-1)))
 
+#meas_noise_xy = np.random.normal
 #bot was always moving forward and rotating a little bit.
 meas = {}
 #add relative odom constraints (noiseless for now)
 for i in range(N-1):
     th = (pos[2,i+1] - pos[2,i]    +np.pi)%(2*np.pi)-np.pi
     rel_trans = rotmat(pos[2,i]).T @ (pos[:2,i+1] - pos[:2,i])
-    meas[('b',i,i+1)] = np.asarray([[rel_trans[0],rel_trans[1],th]]).T
+    meas[('b',i,i+1)] = np.asarray([[rel_trans[0],rel_trans[1],th]]).T# + np.random.normal(scale=0.01,size=((3,1)))
 meas[('b',0,N-1)] = np.zeros((3,1))
 meas[('p',0,)] = pos[:,0,np.newaxis]
 rpos = np.vstack((pos[0,:],pos[1,:],np.cos(pos[2,:]),np.sin(pos[2,:])))
+
+posC = hull_so2(pos)
+measC = graph_hull_so2(meas)
 
 #direct gradient descent solution
 pn = np.zeros((3,N))
@@ -27,34 +31,65 @@ COV = np.diag([0.1,0.1,1.0])
 CINV = np.linalg.inv(COV)
 
 
+x0 = posC + np.random.normal(scale=1.0,size=posC.shape)
+#x0 = np.zeros_like(posC)
+x0[:,0] = posC[:,0]
 
-def H_all(state, meas, no_jac=False):
-    loss = 0.0
-    grad_H  = np.zeros_like(state)
-    for m in meas:
-        if m[0] == 'b':
-            #pose-pose measurement
-            if no_jac:
-                loss += H_between2(state[:,m[1]],state[:,m[2]],meas[m],CINV=CINV,no_jac=no_jac)
-            else:
-                l, jac_i, jac_j = H_between2(state[:,m[1]],state[:,m[2]],meas[m],CINV=CINV,no_jac=no_jac)
-                grad_H[:,m[1]] += jac_i[0]
-                grad_H[:,m[2]] += jac_j[0]
-                loss += l
-        if m[0] == 'p':
-            #prior
-            if no_jac:
-                loss += H_prior2(state[:,m[1]], meas[m], CINV=CINV,no_jac=no_jac)
-            else:
-                l, jac_i = H_prior2(state[:,m[1]], meas[m], CINV=CINV,no_jac=no_jac)
-                grad_H[:,m[1]] += jac_i[0]
-                loss += l
-    if no_jac:
-        return loss
-    else:
-        return loss, grad_H
+x0, losses, lams, states = solve_graph_convex(x0,measC)
+
+final_angles = np.arctan2(x0[3,:],x0[2,:])
+
+plt.subplot(1,2,1)
+plt.plot(losses)
+plt.yscale('log')
+plt.subplot(1,2,2)
+plt.plot(lams)
+plt.yscale('log')
+plt.figure()
+plt.plot(pos[0,:],pos[1,:], 'k')
+plt.plot(x0[0,:],x0[1,:],'b')
+plt.gca().set_aspect(1.0)
+plt.figure()
+plt.plot(pos[2,:],color='k')
+plt.plot(final_angles, color='b')
 
 
+fig = plt.figure()
+ax = fig.add_subplot()
+ax.set_aspect(1.0)
+def animate(idx):
+    ax.clear()
+    ax.plot(pos[0,:],pos[1,:], 'k')
+    ax.plot(states[idx][0,:],states[idx][1,:], 'b')
+    ax.set_title(idx)
+import matplotlib.animation as ani
+anim = ani.FuncAnimation(fig, animate, frames=range(0,len(states),1))
+plt.show()
+
+
+plt.show()
+
+
+
+# state0 = np.asarray([0.0,0.0,1.0,0.0])
+# state1 = np.asarray([1.0,0.0,1.0,0.0])
+# meas = np.asarray([1.0,0.0,1.0,1.0])[:,np.newaxis]
+# dist = 2.0
+# npt = 200
+# c_space = np.asarray(np.meshgrid(np.linspace(-dist,dist,npt),np.linspace(-dist,dist,npt)))
+
+# losses = np.zeros((npt,npt))
+
+# for si in range(npt):
+#     for ci in range(npt):
+#         losses[si,ci] = H_between2_conv(state0,np.asarray([c_space[1,si,ci],c_space[0,si,ci],1.0,0.6]),meas,1.0,1.0)
+
+# fig = plt.figure()
+# ax = fig.add_subplot()
+# ax.imshow(losses.T, origin='lower', extent = [-dist,dist,-dist,dist])
+# plt.show()
+
+''' LMQ solver
 #all-zero initialization fails to find the global minimum.
 x0 = np.zeros((3,N))
 
@@ -91,7 +126,8 @@ for i in tqdm.trange(1000):
     
     x0 = upd
     states.append(np.copy(x0))
-        
+   
+     
 plt.subplot(1,2,1)
 plt.plot(losses)
 plt.yscale('log')
@@ -117,3 +153,40 @@ anim = ani.FuncAnimation(fig, animate, frames=range(0,len(states),10))
 plt.show()
 #plt.plot(x,y)
 #plt.show()
+'''
+
+''' #Scratch code for derivative test
+
+
+npt = 500
+lam = np.linspace(-np.pi,np.pi,npt)
+linesp = np.linspace(-1,1,100)
+fvals = np.zeros_like(lam)
+
+mth = np.random.uniform()*2*np.pi
+
+X0 = np.random.normal(size=(4,31))
+
+meas = np.asarray([np.random.normal(),np.random.normal(),np.cos(mth),np.sin(mth)])[:,np.newaxis]
+for i in range(npt):
+    X0[3,1] = lam[i]
+    fvals[i] = HC_all(X0,measC)[0]
+
+pnt = -2.0
+
+X0[3,1] = pnt
+y, der = HC_all(X0,measC)
+
+line = linesp*der[3,1] + y
+
+fig = plt.figure()
+ax = fig.add_subplot()
+ax.plot(lam,fvals)
+ax.plot(linesp+pnt,line)
+#print(HC_all(posC,measC))
+
+plt.show()
+#print(HC_all(np.zeros((4,posC.shape[1])),measC))
+
+
+'''
